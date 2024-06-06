9483a8e5e1016043f03fa75b970f51b0e38df023
Rajath Hanugul Sudhakar <rajath.hanugul@perficient.com>
2024-06-06 14:39:53 +0530

    Updated hero and other section

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const headerElement = document.getElementById("header");
 const heroElement = document.getElementById("hero");
-
+const aboutMeElement = document.getElementById("about");
+const factElement = document.getElementById("facts");
    // Use XMLHttpRequest for older browsers
 
 function loadSectionContent(elementId, filePath) {
@@ -21,8 +22,17 @@ function loadSectionContent(elementId, filePath) {
 //     })
 //     .catch(error => console.error(error))
 
-loadSectionContent(heroElement, "main/hero/hero.html");
-loadSectionContent(headerElement, "main/header/header.html");
+loadSectionContent(heroElement, "main/hero.html");
+loadSectionContent(headerElement, "main/header.html");
+loadSectionContent(aboutMeElement, "main/aboutMe.html");
+loadSectionContent(factElement, "main/facts.html");
+loadSectionContent(skills, "main/skills.html");
+loadSectionContent(resume, "main/resume.html");
+// loadSectionContent(portfolio, "main/portfolio.html");
+// loadSectionContent(services, "main/services.html");
+// loadSectionContent(testimonials, "main/testimonials.html");
+loadSectionContent(contact, "main/contact.html");
+loadSectionContent(footer, "main/footer.html");
 
 
 
